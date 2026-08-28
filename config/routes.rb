f459505edfbf50f0610cbc1d8b6c5380e0448a76
haruki muroi <OCA2602IT0015@edu.oca.ac.jp>
2026-08-28 06:51:58 +0000

    Initial commit (Rails setup)

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
 
-  # 商品登録
-  get 'products/new'
-  post 'products', to: 'products#create'
+  # # 商品登録
+  # get 'products/new'
+  # post 'products', to: 'products#create'
 
-  # 商品一覧
-  get 'products', to: 'products#index'
+  # # 商品一覧
+  # get 'products', to: 'products#index'
 
-  # 商品詳細
-  get 'products/:id', to: 'products#show', as: 'product'
+  # # 商品詳細
+  # get 'products/:id', to: 'products#show', as: 'product'
 
-  # 商品編集
-  get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
-  patch 'products/:id', to: 'products#update'
+  # # 商品編集
+  # get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
+  # patch 'products/:id', to: 'products#update'
 
-  # 商品削除
-  delete 'products/:id', to: 'products#destroy', as: 'destroy_product'
+  # # 商品削除
+  # delete 'products/:id', to: 'products#destroy', as: 'destroy_product'
 
   # 商品関連
-  # resources :products
+  resources :products
 
   # トップページ
   root to: "homes#top"
